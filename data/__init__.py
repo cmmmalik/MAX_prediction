91d1dcb66db8b09846bf81b6a56d0754f950d4b3
cmmmalik <malik@mm.tu-darmstadt.de>
2024-01-22 12:14:13 +0100

    First: creation of __init__.py in data

---
+++ data/__init__.py
@@ -0,0 +1,23 @@
+from pandas import read_csv
+import io
+import os
+
+filename = os.path.join(os.path.dirname(__file__), "./NBS_thermochemical_data.csv")
+
+
+def read_file(specie:str, file=None):
+
+    if not file:
+        file = filename
+    data = ""
+    with open(file, "r") as ff:
+        for line in ff:
+            if line.startswith(f"# {specie}"):
+                for ll in ff:
+                    if ll.startswith(f"#"):
+                        break
+                    data += ll
+                break
+
+    csvstr = io.StringIO(data)
+    return read_csv(csvstr, delimiter=",")
