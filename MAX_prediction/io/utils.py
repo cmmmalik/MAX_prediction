87c34d38c88ca16957dc45c7c0d31ba4cb1c1af0
cmmmalik <malik@mm.tu-darmstadt.de>
2024-01-23 16:19:55 +0100

    Add: filter_lowesten_mongodb_entries for filtering and selecting lowest energy entry

--- MAX_prediction/io/utils.py
+++ MAX_prediction/io/utils.py
@@ -28,3 +28,17 @@ def read_pickletargz(obj:MXeneAnalyzers_beta, tmpfolder=".reactions"):
 def readhdf5(obj:MXeneAnalyzers_beta, file, colnames=None):
     hdf5logger = HDF5LoggerCollections(file=file, obj=obj)
     hdf5logger.read_from_hdffile(colnames=colnames)
+
+
+def filter_lowesten_mongodb_entries(elentries:dict, warn=True):
+    for k, v in elentries.items():
+        if len(v) > 1:
+            if warn:
+                warnings.warn("More than one entry was found for the element: {}".format(k), RuntimeWarning)
+            print("These are the full data of the entries being sorted and filtered:\n{}".format(v))
+            print("Sorting and selecting the lowest energy entry")
+            #sorting here and selecting the lowest energy
+            v.sort(key=lambda entry: entry.energy_per_atom)
+            print('sorted list based on energy_per_atom of entry list are:')
+            print([f"Entry({entry.entry_id},{entry.energy_per_atom})" for entry in v])
+            elentries[k] = [v[0]]
