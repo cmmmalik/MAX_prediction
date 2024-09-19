5f279810a10889ca4b91be6de50e6b4b6f726935
cmmmalik <malik@mm.tu-darmstadt.de>
2024-09-19 10:37:18 +0200

    Add: compounds elemental combinations

--- MAX_prediction/utils.py
+++ MAX_prediction/utils.py
@@ -55,3 +55,43 @@ class Genchemicalsystems:
     def gen_unique_possible_combinations(self):
         sizes = [i for i in range(2, len(self.elements)+1)]
         return self.unique_combinations_sizes(sizes=sizes)
+
+
+def iter_enum_combine_compounds(compounds: list, combinations: int, necessary=None, subset=None):
+    gen = itcombinations(enumerate(compounds), combinations)
+
+    if necessary is None and subset is None:
+        for comp in gen:
+            yield comp
+    else:
+        if necessary is not None:
+
+            for comp in gen:
+                els = {e for _,com in comp for e in re.findall("[A-Z][a-z]?", com)}
+                # els = list(els)
+                # els.sort()
+
+                if necessary != els:
+                    continue
+
+                yield comp
+
+        elif subset is not None:
+            subset = set(subset)
+            for comp in gen:
+                els = {e for i,com in comp for e in re.findall("[A-Z][a-z]?", com)}
+                if els.issubset(subset) or els.issuperset(subset):
+                    yield comp
+
+
+def combine_enum_compounds_multisize(compounds: list, combination_size: list, necessary=None, subset=None):
+    if subset is not None:
+        assert not necessary
+
+    elif necessary is not None:
+        necessary = set(necessary)
+        assert not subset
+
+    for comb in combination_size:
+        for comps in iter_enum_combine_compounds(compounds, combinations=comb, necessary=necessary, subset=subset):
+            yield comps
