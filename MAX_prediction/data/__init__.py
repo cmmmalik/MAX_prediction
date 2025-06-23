9dbae82860f50436c70a38bbccbe03e5f45a810d
cmmmalik <malik@mm.tu-darmstadt.de>
2025-06-23 14:44:23 +0200

    Add read_file_nbscsv function to read specific species data from CSV files

--- MAX_prediction/data/__init__.py
+++ MAX_prediction/data/__init__.py
@@ -21,3 +21,19 @@ def read_file(specie:str, file=None):
 
     csvstr = io.StringIO(data)
     return read_csv(csvstr, delimiter=",")
+
+def read_file_nbscsv(specie:str, file):
+    data = ""
+    #get the header...
+    columns = "Formula,Solvent,Name,State Description,State,Molar Mass, deltafHf0°,deltafH°,deltafG°,H°-H0°,S°,Cp"
+    with open(file, "r") as ff:
+        for line in ff:
+            if line.startswith(f"{specie}"):
+                for ll in ff:
+                    if ll.startswith("Table"):
+                        break
+                    data += ll
+                break
+
+    csvstr = io.StringIO(data)
+    return read_csv(csvstr, delimiter=",", columns=columns.split(","))
